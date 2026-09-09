049623a0e6341edcb28c2b2d9be86f15c740dd05
445kefu-dot <445kefu@gmail.com>
2026-09-10 06:26:43 +0800

    Update webhook URL to use static token
    Replaced dynamic webhook URL with a static token.

--- mian.py
+++ mian.py
@@ -323,7 +323,7 @@ def main():
         listen='0.0.0.0',
         port=port,
         url_path=BOT_TOKEN,
-        webhook_url = os.environ.get('WEBHOOK_URL', f"https://xiafa.onrender.com/{BOT_TOKEN}")
+        webhook_url = f"https://xiafa.onrender.com/8825283140:AAEW53jACQKb1pwGDN5-6ASKKEhWdQf6dvs"
     )
 
 if __name__ == "__main__":
